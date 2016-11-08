7fbe4b32892932d10e892d760b2397f0aa1a0bf4
Lars-Thestorf <lars.thestorf@web.de>
2016-11-08 14:33:40 +0100

    Added odd and even feature
    If you use the --odd argument only odd lines will be shown
    and if you use the --even argument only even lines will be shown

--- readfile.go
+++ readfile.go
@@ -10,11 +10,13 @@ import (
 )
 
 func main() {
-	striPtr := flag.String("path", "", "path of the file to read")
-	boolPtr := flag.Bool("linenumbers", false, "Show Line Numbers")
-	
+	pathPtr := flag.String("path", "", "path of the file to read")
+	linePtr := flag.Bool("linenumbers", false, "Show Line Numbers")
+	odd_Ptr := flag.Bool("odd", false, "Only show odd Lines")
+	evenPtr := flag.Bool("even", false, "Only show even Lines")
 	flag.Parse()
-	file, err := os.Open(*striPtr)
+	
+	file, err := os.Open(*pathPtr)
 	if err!=nil{
 		log.Fatal(err)
 	}
@@ -22,11 +24,17 @@ func main() {
 	i:=0
     for scanner.Scan() {
 		i++
-		if (*boolPtr) {
-			fmt.Println(strconv.Itoa(i)+") "+scanner.Text())
-		} else {
-			fmt.Println(scanner.Text())
+		if (Even(i)&&*evenPtr)||(!Even(i)&&*odd_Ptr)||(!*odd_Ptr&&!*evenPtr){
+			if *linePtr {
+				fmt.Println(strconv.Itoa(i)+") "+scanner.Text())
+			} else {
+				fmt.Println(scanner.Text())
+			}
 		}
     }
 	file.Close()
+}
+
+func Even(number int) bool {
+    return number%2 == 0
 }
