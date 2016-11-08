ec51f682f38ffbcb44911edd878176a121679bd4
Lars-Thestorf <lars.thestorf@web.de>
2016-11-08 14:02:20 +0100

    linenumber feature
    Programm shows you the line numbers if you use --linenumbers

--- readfile.go
+++ readfile.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"bufio"
     "log"
     "os"
+	"strconv"
 )
 
 func main() {
-	file, err := os.Open("file.txt")
+	striPtr := flag.String("path", "", "path of the file to read")
+	boolPtr := flag.Bool("linenumbers", false, "Show Line Numbers")
+	
+	flag.Parse()
+	file, err := os.Open(*striPtr)
 	if err!=nil{
 		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(file)
+	i:=0
     for scanner.Scan() {
-        fmt.Println(scanner.Text())
+		i++
+		if (*boolPtr) {
+			fmt.Println(strconv.Itoa(i)+") "+scanner.Text())
+		} else {
+			fmt.Println(scanner.Text())
+		}
     }
 	file.Close()
 }
