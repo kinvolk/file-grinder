db59136da0416aacd7919a2eb2ae62d25d43fc25
Lars-Thestorf <lars.thestorf@web.de>
2016-11-09 11:17:53 +0100

    Add linenumber option
    add `--linenumber` option that will show linenumbers in the output
    refactor some code

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func check(e error) {
@@ -14,21 +15,32 @@ func check(e error) {
 	}
 }
 
-func readafile(path string) string {
+func readafile(path string) []string {
 	file, err := os.Open(path)
 	check(err)
-	str := ""
+	var str []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		str += scanner.Text()
+		str = append(str, scanner.Text())
 	}
 	file.Close()
-	return string(str)
+	return str
 }
 
 func main() {
 	pathtofile := flag.String("path", "", "please declare the path of your file.")
+	linenumbers := flag.Bool("linenumbers", false, "Show the linenumbers")
 	flag.Parse()
 	data := readafile(*pathtofile)
-	fmt.Println(string(data))
+	print(data, *linenumbers)
+}
+
+func print(data []string, numb bool) {
+	for s := range data {
+		if numb {
+			fmt.Println(strconv.Itoa(s+1) + ") " + data[s])
+		} else {
+			fmt.Println(data[s])
+		}
+	}
 }
